95777e471c81c60905edfb1af34a9c1c6bcedba9
jcheng2020-github <69835036+jcheng2020-github@users.noreply.github.com>
2022-10-05 01:19:40 -0400

    A example Arduino Uno sketch

---
+++ Servo.ino
@@ -0,0 +1,29 @@
+#include "vexServo.hpp"
+#include "Servo.h"
+
+int vexServoPin = 9;
+vexServo motor;
+
+void setup() {
+  // put your setup code here, to run once:
+  motor.initial(vexServoPin);
+  
+  motor.setPosition(6);
+  delay(1000);
+  motor.setPosition(0);
+  delay(1000);
+  motor.setPosition(3);
+  delay(1000);
+  motor.setPosition(0);
+  delay(1000);
+  motor.setPosition(3);
+  delay(1000);
+  motor.setPosition(6);
+  delay(1000);
+  motor.setPosition(0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
